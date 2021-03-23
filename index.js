3abc5d90ae15b5529ce62448b822c23530f2a7e3
JasonQuinalayo <quinalayojason@gmail.com>
2021-03-23 16:45:51 -0400

    init

--- index.js
+++ index.js
@@ -1,87 +1,89 @@
-require('dotenv').config()
+/* eslint-disable consistent-return */
+require('dotenv').config();
 const express = require('express');
+
 const app = express();
 const morgan = require('morgan');
-app.use(express.json());
-morgan.token('log-post', (req, res) => req.method === 'POST' ? JSON.stringify(req.body) : '')
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :log-post'))
-app.use(express.static('build'))
 
-const Person = require('./models/person')
+app.use(express.json());
+morgan.token('log-post', (req) => (req.method === 'POST' ? JSON.stringify(req.body) : ''));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :log-post'));
+app.use(express.static('build'));
+
+const Person = require('./models/person');
 
 app.get('/api/persons', (req, res, next) => {
-  Person.find({}).then(result => {
-    res.json(result)
+  Person.find({}).then((result) => {
+    res.json(result);
   })
-  .catch(err=>next(err))
-})
+    .catch((err) => next(err));
+});
 
 app.get('/info', (req, res, next) => {
-  Person.find({}).then(result => {
-    res.send(`Phonebook has info for ${result.length} people <br /> ${new Date()}`)
+  Person.find({}).then((result) => {
+    res.send(`Phonebook has info for ${result.length} people <br /> ${new Date()}`);
   })
-  .catch(err=>next(err))
-})
+    .catch((err) => next(err));
+});
 
 app.get('/api/persons/:id', (req, res, next) => {
-  Person.findById(req.params.id).then(result => {
-    if (result) res.json(result)
-    else res.status(404).end()
+  Person.findById(req.params.id).then((result) => {
+    if (result) res.json(result);
+    else res.status(404).end();
   })
-  .catch(err=>next(err))
-})
+    .catch((err) => next(err));
+});
 
 app.delete('/api/persons/:id', (req, res, next) => {
-  Person.findByIdAndDelete(req.params.id).then(result => {
-    res.status(204).end()
+  Person.findByIdAndDelete(req.params.id).then(() => {
+    res.status(204).end();
   })
-  .catch(err=>next(err))
-})
+    .catch((err) => next(err));
+});
 
 app.post('/api/persons', (req, res, next) => {
   if (!req.body.name || !req.body.number) {
-    return res.status(400).send('Missing name or number')
+    return res.status(400).send('Missing name or number');
   }
-  Person.find({name: req.body.name}).then(result => {
+  Person.find({ name: req.body.name }).then((result) => {
     if (result.length !== 0) {
-      return res.status(400).send(`${req.body.name} already in the phonebook!`)
-    } else {
-        const person = {
-          name: req.body.name,
-          number: req.body.number
-        }
-        Person.create(person)
-          .then(()=>res.send(person))
-          .catch(err => next(err))
+      return res.status(400).send(`${req.body.name} already in the phonebook!`);
     }
+    const person = {
+      name: req.body.name,
+      number: req.body.number,
+    };
+    Person.create(person)
+      .then(() => res.send(person))
+      .catch((err) => next(err));
   })
-  .catch(err=>next(err))
-})
+    .catch((err) => next(err));
+});
 
 app.put('/api/persons/:id', (req, res, next) => {
-  console.log(req.body)
-  Person.findByIdAndUpdate(req.params.id, {number: req.body.number}, {runValidators: true, new :true})
-    .then(result => res.json(result))
-    .catch(err=>next(err))
-})
+  Person.findByIdAndUpdate(
+    req.params.id, { number: req.body.number }, { runValidators: true, new: true },
+  )
+    .then((result) => res.json(result))
+    .catch((err) => next(err));
+});
 
 app.use((req, res) => {
-  res.status(404).send({error: 'Unknown endpoint'})
-})
+  res.status(404).send({ error: 'Unknown endpoint' });
+});
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({error: error.message})
+    return response.status(400).send({ error: 'malformatted id' });
+  } if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
   }
-  next(error)
-}
+  next(error);
+};
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`)
-})
+  console.log(`Server listening on port ${PORT}`);
+});
--- models/person.js
+++ models/person.js
@@ -1,37 +1,43 @@
-const mongoose = require('mongoose')
+/* eslint-disable linebreak-style */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-console */
+const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
-console.log('connecting to', url)
+console.log('connecting to', url);
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(result => {
-    console.log('connected to MongoDB')
+mongoose.connect(url, {
+  useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true,
+})
+  .then(() => {
+    console.log('connected to MongoDB');
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    console.log('error connecting to MongoDB:', error.message);
+  });
 
 const personSchema = new mongoose.Schema({
-  name: {type: String, minlength: 3, required: true},
+  name: { type: String, minlength: 3, required: true },
   number: {
-    type: String, 
+    type: String,
     validate: {
-      validator: (v)=>/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/.test(v),
-      message: 'invalid phone number'
-    }
-    ,required: true 
-  }
-})
-personSchema.plugin(uniqueValidator)
+      validator: (v) => /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/.test(v),
+      message: 'invalid phone number',
+    },
+    required: true,
+  },
+});
+personSchema.plugin(uniqueValidator);
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model('Person', personSchema);
